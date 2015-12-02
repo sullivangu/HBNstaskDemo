97e13a100dd537c20fbdb181612c01d99ec430e1
Sullivan.Gu <sullivangu@mobile-packagetekiMac-mini.local>
2015-12-02 10:37:55 +0800

    ini

---
+++ HBNstaskDemo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  HBNstaskDemo
+//
+//  Created by Sullivan.Gu on 15/12/2.
+//  Copyright © 2015年 Sullivan.Gu. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ HBNstaskDemo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HBNstaskDemo
+//
+//  Created by Sullivan.Gu on 15/12/2.
+//  Copyright © 2015年 Sullivan.Gu. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
